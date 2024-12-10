58757f973367bd8420151e37d13c5b5a22102bcf
TomWall1 <thomasjameswall1@gmail.com>
2024-12-10 16:31:38 +1100

    Update FileUpload.jsx

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -29,9 +29,6 @@ export function FileUpload({ onFileSelected, accept = '.csv', label = 'Upload CS
                  hover:border-blue-500 hover:bg-blue-50 transition-colors
                  flex items-center justify-center gap-2 w-full"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 6.707a1 1 0 010-1.414l3-3a1 1 0 011.414 0l3 3a1 1 0 01-1.414 1.414L11 5.414V13a1 1 0 11-2 0V5.414L7.707 6.707a1 1 0 01-1.414 0z" clipRule="evenodd" />
-        </svg>
         {label}
       </button>
     </div>
