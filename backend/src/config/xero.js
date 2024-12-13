4b3e1f9508b50a73c23f27795d9c5dfa5a9bf8f5
TomWall1 <thomasjameswall1@gmail.com>
2024-12-13 21:02:04 +1100

    Update xero.js

--- backend/src/config/xero.js
+++ backend/src/config/xero.js
@@ -1,12 +1,21 @@
 import { XeroClient } from 'xero-node';
 
-const defaultScopes = 'offline_access accounting.transactions.read accounting.contacts.read';
-
 const xeroConfig = {
   clientId: process.env.XERO_CLIENT_ID,
   clientSecret: process.env.XERO_CLIENT_SECRET,
   redirectUris: [process.env.XERO_REDIRECT_URI],
-  scopes: (process.env.XERO_SCOPES || defaultScopes).split(' ')
+  scopes: process.env.XERO_SCOPES?.split(' ') || [
+    'offline_access',
+    'accounting.transactions.read',
+    'accounting.contacts.read'
+  ]
 };
+
+console.log('Xero Config:', {
+  clientId: process.env.XERO_CLIENT_ID ? 'Set' : 'Missing',
+  clientSecret: process.env.XERO_CLIENT_SECRET ? 'Set' : 'Missing',
+  redirectUri: process.env.XERO_REDIRECT_URI,
+  scopes: xeroConfig.scopes
+});
 
 export const xeroClient = new XeroClient(xeroConfig);
