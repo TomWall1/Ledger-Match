8d0e6ffb8b56a2189b14d5ba6540bf2a15dec868
TomWall1 <thomasjameswall1@gmail.com>
2025-02-17 19:18:51 +1100

    Restore full application code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,14 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import XeroAuth from './components/XeroAuth';
-import XeroCallback from './components/XeroCallback';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/auth/xero" element={<XeroAuth />} />
-        <Route path="/auth/xero/callback" element={<XeroCallback />} />
-        <Route
-          path="/*"
-          element={<div>Hello World</div>}
-        />
-      </Routes>
-    </Router>
-  );
-};
-
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+reportWebVitals();
