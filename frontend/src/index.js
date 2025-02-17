391bacfab9b0785664dbc9646c1cc927a1a3d5ec
TomWall1 <thomasjameswall1@gmail.com>
2025-02-17 19:13:41 +1100

    Test with simplified index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,29 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import XeroAuth from './components/XeroAuth';
+import XeroCallback from './components/XeroCallback';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/auth/xero" element={<XeroAuth />} />
+        <Route path="/auth/xero/callback" element={<XeroCallback />} />
+        <Route
+          path="/*"
+          element={<div>Hello World</div>}
+        />
+      </Routes>
+    </Router>
+  );
+};
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
