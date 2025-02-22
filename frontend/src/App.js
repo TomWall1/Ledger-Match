60c19204a2e9898c7c6af3a0461b8e5fee23e92f
TomWall1 <thomasjameswall1@gmail.com>
2025-02-23 08:49:17 +1100

    Update App.js imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import XeroAuth from './components/XeroAuth';
+import XeroCallback from './components/XeroCallback';
+
+// ... rest of the MainApp component code ...
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/auth/xero" element={<XeroAuth />} />
         <Route path="/auth/xero/callback" element={<XeroCallback />} />
-        <Route path="/test" element={<TestUpload />} />
         <Route path="/*" element={<MainApp />} />
       </Routes>
     </Router>
