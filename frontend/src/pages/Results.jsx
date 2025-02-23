06abec8f82907c6d0792e5391d7b206e2346f55c
TomWall1 <thomasjameswall1@gmail.com>
2025-02-23 13:22:16 +1100

    Add Results page

---
+++ frontend/src/pages/Results.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import MatchingResults from '../components/MatchingResults';
+
+const Results = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const results = location.state?.results;
+
+  if (!results) {
+    navigate('/upload');
+    return null;
+  }
+
+  return (
+    <div className="min-h-screen bg-gray-50 p-6">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="text-3xl font-bold text-[#1B365D]">Matching Results</h1>
+          <div className="flex space-x-4">
+            <Link
+              to="/upload"
+              className="flex items-center px-4 py-2 bg-[#00A4B4] text-white rounded-lg hover:bg-[#008999] transition-colors"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 6.707a1 1 0 010-1.414l3-3a1 1 0 011.414 0l3 3a1 1 0 01-1.414 1.414L11 5.414V13a1 1 0 11-2 0V5.414L7.707 6.707a1 1 0 01-1.414 0z" clipRule="evenodd" />
+              </svg>
+              Upload New Files
+            </Link>
+            <Link
+              to="/"
+              className="flex items-center text-[#1B365D] hover:text-[#00A4B4] transition-colors"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
+              </svg>
+              Back to Home
+            </Link>
+          </div>
+        </div>
+
+        <MatchingResults matchResults={results} />
+      </div>
+    </div>
+  );
+};
+
+export default Results;
