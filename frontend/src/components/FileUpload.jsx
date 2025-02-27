ec042aa47efa6ecd954c1c8289f1ca3e4e2f6246
TomWall1 <thomasjameswall1@gmail.com>
2025-02-27 21:40:02 +1100

    Fix file upload button styling to use navy blue background

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -22,7 +22,7 @@ export const FileUpload = ({ onFileSelected, accept, label }) => {
       />
       <button
         onClick={() => fileInput.current.click()}
-        className="flex items-center px-4 py-2 bg-primary-navy text-white rounded-lg hover:bg-opacity-90 transition-colors"
+        className="flex items-center px-4 py-2 bg-[#1B365D] text-white rounded-lg hover:bg-opacity-90 transition-colors"
       >
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 6.707a1 1 0 010-1.414l3-3a1 1 0 011.414 0l3 3a1 1 0 01-1.414 1.414L11 5.414V13a1 1 0 11-2 0V5.414L7.707 6.707a1 1 0 01-1.414 0z" clipRule="evenodd" />
